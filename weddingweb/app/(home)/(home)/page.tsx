67a4efee3bb557432a200e32cdc03f6645859608
Papa Yaw Agyeman-Manu <agy20002@byui.edu>
2024-01-11 18:50:32 -0700

    css updates to homepage

--- weddingweb/app/(home)/(home)/page.tsx
+++ weddingweb/app/(home)/(home)/page.tsx
@@ -44,34 +44,49 @@ export default async function Home() {
   });
 
   // console.log(userInfo);
+  // bg-[url('/resize/rsz_1ring.jpg')] bg-cover
 
   return (
     <>
-      <div className=" relative h-full w-full flex flex-col p-4 m-auto overflow-y-auto bg-[url('/resize/rsz_1ring.jpg')] bg-cover text-white md:text-black md:bg-none md:bg-amber-50">
+      <div className=" relative h-full w-full flex flex-col p-4 m-auto overflow-y-auto text-white md:text-black bg-amber-50">
         <Image
-          className="md:block absolute aspect-square rounded-lg z-10 shadow-2xl hidden h-[250px] w-[250px] top-[50px] right-[70px] rotate-[25deg]"
+          className="absolute aspect-square rounded-lg z-10 shadow-2xl h-[180px] w-[180px] top-[50px] right-[50px] rotate-[25deg] md:h-[250px] md:w-[250px] md:top-[50px] md:right-[70px] md:rotate-[25deg]"
           src={"/resize/rsz_1sign-in-page.jpg"}
           width={300}
           height={300}
           alt="holding image"
         />
         <Image
-          className="md:block absolute aspect-square rounded-lg z-0 shadow-2xl hidden h-[250px] w-[250px] bottom-[40px] right-[200px] -rotate-12"
+          className="absolute aspect-square rounded-lg z-0 shadow-2xl h-[180px] w-[180px] bottom-[370px] right-[15px] -rotate-12 md:h-[250px] md:w-[250px] md:bottom-[40px] md:right-[200px] md:-rotate-12"
           src={"/resize/rsz_1holding hands.jpg"}
           width={300}
           height={300}
           alt="holding image"
         />
         <Image
-          className="md:block absolute aspect-square rounded-lg z-10 shadow-2xl hidden h-[250px] w-[250px] top-[50px] left-[70px] -rotate-[25deg]"
+          className="absolute aspect-square rounded-lg z-10 shadow-2xl h-[180px] w-[180px] top-[10px] left-[30px] -rotate-[25deg] md:h-[250px] md:w-[250px] md:top-[50px] md:left-[70px] md:-rotate-[25deg]"
           src={"/resize/rsz_standingtogether_2.jpg"}
           width={300}
           height={300}
           alt="holding image"
         />
         <Image
-          className="md:block absolute aspect-square rounded-lg z-0 shadow-2xl hidden h-[250px] w-[250px] bottom-[40px] left-[200px] rotate-12"
+          className="absolute aspect-square rounded-lg z-0 shadow-2xl h-[180px] w-[180px] bottom-[370px] left-[15px] rotate-12 md:h-[250px] md:w-[250px] md:bottom-[40px] md:left-[200px] md:rotate-12"
           src={"/resize/rsz_standingtogether_1.jpg"}
+          width={300}
+          height={300}
+          alt="holding image"
+        />
+        <Image
+          className="absolute aspect-square rounded-lg z-0 shadow-2xl h-[180px] w-[180px] bottom-[35px] left-[5px] rotate-12 md:hidden"
+          src={"/resize/rsz_tree_hug.jpg"}
+          width={300}
+          height={300}
+          alt="holding image"
+        />
+        <Image
+          className="absolute aspect-square rounded-lg z-0 shadow-2xl h-[200px] w-[200px] bottom-[200px] left-[120px] md:hidden"
+          src={"/resize/rsz_1ring.jpg"}
           width={300}
           height={300}
           alt="holding image"
@@ -84,7 +99,7 @@ export default async function Home() {
             Thank you for visiting our website
           </p>
         </div>
-        <div className="flex h-full p-2 justify-center items-center z-40 bg-black rounded-lg bg-opacity-20 md:bg-transparent md:rounded-none md:bg-opacity-100 ">
+        <div className="flex h-full p-2 justify-center items-center z-40 bg-black rounded-lg bg-opacity-25 md:bg-transparent md:rounded-none md:bg-opacity-100 ">
           <h1 className={`text-[2em] ${robotoSerif.className}`}>
             We&apos;re getting married
           </h1>
